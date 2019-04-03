83539812a80cbb42cdc455dbdf8d8eae0d499928
红白 <hongbai@gegejia.com>
2019-04-03 11:04:11 +0800

    add antd

---
+++ src/entry.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Button} from 'antd'
+import './App.css';
+
+class GoAdmin extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">
+          按钮
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default GoAdmin;
