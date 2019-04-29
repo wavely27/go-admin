9103ffe02574ee3bf6e9dfe57498571294e3579e
红白 <hongbai@gegejia.com>
2019-04-29 15:57:23 +0800

    update

--- src/Admin/FormView/index.js
+++ src/Admin/FormView/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, {Component} from 'react'
 import {
   Form, Row, Col, Button, Icon,
@@ -6,7 +5,6 @@ import {
 import PropTypes from "prop-types";
 import getItem from './getItem'
 import styles from './style.module.css'
-
 
 const {Item} = Form
 
@@ -39,7 +37,9 @@ class FormContent extends Component {
     const {core, formConfig, filterConfig} = props
 
     // default
-    let formLayout, btn, getFormEleList
+    let formLayout
+    let btn
+    let getFormEleList
 
     if (formConfig) { // 表单 default
       const {onOk, onBack, options = {}} = formConfig
@@ -73,7 +73,7 @@ class FormContent extends Component {
         more = (
           <a style={{marginLeft: 8, fontSize: 12}} onClick={this.toggle}>
             更多
-            <Icon type={this.state.expand ? 'up' : 'down'}/>
+            <Icon type={this.state.expand ? 'up' : 'down'} />
           </a>
         )
         const {colCount} = options
@@ -110,213 +110,6 @@ class FormContent extends Component {
     })
   }
 
-  /*  getItems = () => {
-      const {props, state} = this
-      const {formConfig, filterConfig, core, form} = props
-      const {action} = state
-      const config = formConfig || filterConfig
-      let formFlag = false
-      if (formConfig) {
-        formFlag = true
-      }
-
-      const {getFieldDecorator} = props.form;
-
-      const children = config.form.map((item, i) => {
-        const {ifAction} = item
-
-        const thisItem = ifAction && ifAction(form, core)
-        let {label} = {...item, ...thisItem}
-        let wrapLabel
-        const {
-          disable = false,
-          itemKey,
-          prefix,
-          prefixWrapStyle = {},
-          suffix,
-          suffixWrapStyle = {},
-          colProps = {},
-          itemProps = {},
-          fieldProps = {},
-          follow
-        } = {...item, ...thisItem}
-
-        // const {labelStyle = {}} = itemProps
-        const prefixWrap = prefix && <span style={{padding: '0 12 0 12', ...prefixWrapStyle}}>{prefix}</span>
-        const suffixWrap = suffix && <span style={{padding: '0 12 0 12', ...suffixWrapStyle}}>{suffix}</span>
-        const fixLabel = typeof label === 'string'
-          // style={{width: 64, ...labelStyle}}
-          ? <span>{label}</span>
-          : label
-        const labelEle = label && <div style={{display: 'inline-flex',}}>{fixLabel}</div>
-        let child = (
-          <div>{}</div>
-        )
-
-        // style
-        let formItemLayout = {
-          labelCol: {
-            xs: {span: 24},
-            sm: {span: 4},
-          },
-          wrapperCol: {
-            xs: {span: 24},
-            sm: {span: 20},
-          },
-        };
-        let display = 'flex'
-        if (follow || (config.form && config.form[i + 1] && config.form[i + 1].follow)) {
-          display = 'inline-flex'
-          formItemLayout = {}
-          if (!follow) {
-            formItemLayout = {
-              labelCol: {
-                xs: {span: 24},
-                sm: {span: 4},
-              },
-            }
-            wrapLabel = label
-            if (fieldProps.rules) {
-              fieldProps.rules.forEach(rule => {
-                if (rule.required === true) {
-                  wrapLabel = (
-                    <span>
-                      <span>*</span>
-                      {wrapLabel}
-                    </span>
-                  )
-                }
-              })
-            }
-            label = undefined
-          }
-        }
-
-        child = (
-          !disable ? (
-            <Item
-              key={`item-${i}`}
-              label={labelEle}
-              {...formItemLayout}
-              {...itemProps}
-              style={{display, position: 'relative', textAlign: 'left', ...itemProps.style}}
-            >
-              {prefixWrap}
-              {getFieldDecorator(itemKey, {...fieldProps})(
-                getItem({
-                  ...item,
-                  setValue: this.setValue
-                }, formFlag, core, itemKey, action)
-              )}
-              {suffixWrap}
-            </Item>
-          ) : null
-        )
-        return {wrapLabel, follow, colProps, formItemLayout, child}
-      })
-      return children;
-    }
-
-    getCols = () => {
-      // 生成行
-      const {props, state} = this
-      const {formConfig, filterConfig, core, form} = props
-      const config = formConfig || filterConfig
-      const {colCount = 3, layout} = config.options
-      const {getFieldDecorator} = props.form;
-      const {action} = state
-
-
-      // const colNumber =
-      //   layout === 'horizontal'
-      //     ? Math.ceil(24 / colCount)
-      //     : 24
-      // const count = state.expand ? 10 : colCount * 2;
-      //
-      // const itemList = this.getItems()
-      // const loopLength = itemList.length
-      // const fieldList = []
-      // for (let i = 0; i < loopLength; i++) {
-      //   const child = {
-      //     colProps: itemList[i].colProps,
-      //     child: itemList[i].child,
-      //   }
-      //   if (itemList[i + 1] && itemList[i + 1].follow === true) {
-      //     const children = [child]
-      //     for (let j = i + 1; itemList[j].follow === true; j++) {
-      //       const nextChild = {
-      //         colProps: itemList[j].colProps,
-      //         child: itemList[j].child,
-      //       }
-      //       i++
-      //       children.push(nextChild)
-      //     }
-      //     fieldList.push(children)
-      //   } else {
-      //     fieldList.push(child)
-      //   }
-      // }
-      // console.log('fieldList---------', fieldList)
-
-      // const colList = fieldList.map((item, i) => {
-      //   if (item instanceof Array) {
-      //     const {colProps} = item[0]
-      //     const children = item.map(items => items.child)
-      //
-      //     const {formItemLayout} = children[0]
-      //     const wrapped = (
-      //       <div>
-      //         <Col {...formItemLayout}>
-      //           {children[0].wrapLabel}
-      //         </Col>
-      //         <span>
-      //           {children}
-      //         </span>
-      //       </div>
-      //     )
-      //     return (
-      //       <Col
-      //         span={colNumber}
-      //         key={`col-${i}`}
-      //         {...colProps}
-      //         style={{display: layout !== 'horizontal' || i < count ? 'block' : 'none', padding: 0, ...colProps.style}}
-      //       >
-      //         {wrapped}
-      //       </Col>
-      //     )
-      //   } else {
-      //     const {colProps, child} = item
-      //     return (
-      //       <Col
-      //         span={colNumber}
-      //         key={`col-${i}`}
-      //         {...colProps}
-      //         style={{display: layout !== 'horizontal' || i < count ? 'block' : 'none', padding: 0, ...colProps.style}}
-      //       >
-      //         {child}
-      //       </Col>
-      //     )
-      //   }
-      // })
-
-      const colList = config.form.map((item, i) => {
-        const {itemKey, fieldProps, label} = item
-        let formFlag = false
-        return <Item
-          label={label}
-          key={`item-${i}`}
-        >
-          {getFieldDecorator(itemKey, {...fieldProps})(
-            getItem({
-              ...item,
-              setValue: this.setValue
-            }, formFlag, core, itemKey, action)
-          )}
-        </Item>
-      })
-
-      return colList
-    }*/
 
   getDefaultHorizontalItems = () => {
     console.log('getDefaultHorizontalItems-水平')
@@ -333,10 +126,10 @@ class FormContent extends Component {
     const children = config.form.map((item, i) => {
       const {ifAction} = item
       const thisItem = ifAction && ifAction(form, core)
-      let {label} = {...item, ...thisItem}
       const {
         disable = false,
         itemKey,
+        label,
         prefix,
         prefixWrapStyle = {},
         suffix,
@@ -358,7 +151,7 @@ class FormContent extends Component {
         : label
       const labelEle = label && <div className={styles.labelEle}>{fixLabel}</div>
 
-      let child = (
+      const child = (
         <div key={`gItem${i}`} className={styles.itemInlineWrap}>
           <div className={styles.itemFlexTop}>
             {topWrap}
@@ -400,7 +193,7 @@ class FormContent extends Component {
     const {action} = state
     const config = formConfig
 
-    let fixWidth = false
+    const fixWidth = false
 
     const formItemLayout = {
       labelCol: {
@@ -416,10 +209,10 @@ class FormContent extends Component {
     const children = config.form.map((item, i) => {
       const {ifAction} = item
       const thisItem = ifAction && ifAction(form, core)
-      let {label} = {...item, ...thisItem}
       const {
         disable = false,
         itemKey,
+        label,
         prefix,
         prefixWrapStyle = {},
         suffix,
@@ -515,117 +308,6 @@ class FormContent extends Component {
     return children;
   }
 
-  getDefault = () => {
-    const {props, state} = this
-    const {formConfig, filterConfig, core, form} = props
-    const {action} = state
-    const config = formConfig || filterConfig
-    let formFlag = false
-    if (formConfig) {
-      formFlag = true
-    }
-
-    const {getFieldDecorator} = props.form;
-
-    const children = config.form.map((item, i) => {
-      const {ifAction} = item
-
-      const thisItem = ifAction && ifAction(form, core)
-      let {label} = {...item, ...thisItem}
-      let wrapLabel
-      const {
-        disable = false,
-        itemKey,
-        prefix,
-        prefixWrapStyle = {},
-        suffix,
-        suffixWrapStyle = {},
-        colProps = {},
-        itemProps = {},
-        fieldProps = {},
-        follow
-      } = {...item, ...thisItem}
-
-      // const {labelStyle = {}} = itemProps
-      const prefixWrap = prefix && <span style={{padding: '0 12 0 12', ...prefixWrapStyle}}>{prefix}</span>
-      const suffixWrap = suffix && <span style={{padding: '0 12 0 12', ...suffixWrapStyle}}>{suffix}</span>
-      const fixLabel = typeof label === 'string'
-        // style={{width: 64, ...labelStyle}}
-        ? <span>{label}</span>
-        : label
-      const labelEle = label && <div style={{display: 'inline-flex',}}>{fixLabel}</div>
-      let child = (
-        <div>{}</div>
-      )
-
-      // style
-      let formItemLayout = {
-        labelCol: {
-          xs: {span: 24},
-          sm: {span: 4},
-        },
-        wrapperCol: {
-          xs: {span: 24},
-          sm: {span: 20},
-        },
-      };
-      let display = 'flex'
-      if (follow || (config.form && config.form[i + 1] && config.form[i + 1].follow)) {
-        display = 'inline-flex'
-        formItemLayout = {}
-        if (!follow) {
-          formItemLayout = {
-            labelCol: {
-              xs: {span: 24},
-              sm: {span: 4},
-            },
-          }
-          wrapLabel = label
-          if (fieldProps.rules) {
-            fieldProps.rules.forEach(rule => {
-              if (rule.required === true) {
-                wrapLabel = (
-                  <span>
-                    <span>*</span>
-                    {wrapLabel}
-                  </span>
-                )
-              }
-            })
-          }
-          label = undefined
-        }
-      }
-
-      child = (
-        !disable ? (
-          <Item
-            key={`item-${i}`}
-            label={labelEle}
-            {...formItemLayout}
-            {...itemProps}
-            style={{display, position: 'relative', textAlign: 'left', ...itemProps.style}}
-          >
-            {prefixWrap}
-            {getFieldDecorator(itemKey, {...fieldProps})(
-              getItem({
-                ...item,
-                setValue: this.setValue
-              }, formFlag, core, itemKey, action)
-            )}
-            {suffixWrap}
-          </Item>
-        ) : null
-      )
-      return {wrapLabel, follow, colProps, formItemLayout, child}
-    })
-    return children;
-  }
-
-  getItems = () => {
-    return 'abc'
-  }
-
   handleSearch = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
@@ -646,61 +328,6 @@ class FormContent extends Component {
   }
 
   render() {
-    /*    const {core, formConfig, filterConfig} = props
-
-        // default
-        let formLayout, formEleList, btn, test, getformEleList
-
-        if (formConfig) { // 表单 default
-          const {onOk, onBack, options={}} = formConfig
-
-          // result
-          getformEleList = this.getItems
-          btn = (
-            <Row>
-              <Col span={24} style={{textAlign: 'right'}}>
-                <Button type="primary" onClick={() => onOk(props.form, formConfig.params, core)}>保存</Button>
-                <Button style={{marginLeft: 8}} onClick={onBack}>
-                  返回
-                </Button>
-              </Col>
-            </Row>
-          )
-        } else if (filterConfig) { // 过滤器
-          const {options={}, form} = filterConfig
-          // setting
-          formLayout = 'inline'
-          getformEleList = this.getItems
-          // 更多按钮
-          let more = (
-            <a style={{marginLeft: 8, fontSize: 12}} onClick={this.toggle}>
-              更多
-              <Icon type={this.state.expand ? 'up' : 'down'} />
-            </a>
-          )
-
-          if (options.align === true) { // 对齐
-            formLayout = undefined
-            const {colCount} = options
-            const {length} = form
-            more = colCount >= length && null
-            getformEleList = this.getItems
-          }
-
-          // result
-          formEleList = getformEleList()
-          btn = (
-            <Row>
-              <Col span={24} style={{textAlign: 'right'}}>
-                <Button type="primary" onClick={this.handleSearch}>查询</Button>
-                <Button style={{marginLeft: 8}} onClick={this.handleReset}>
-                  重置
-                </Button>
-                {more}
-              </Col>
-            </Row>
-          )
-        }*/
 
     const {formLayout, getFormEleList, btn} = this
 
@@ -709,7 +336,7 @@ class FormContent extends Component {
     console.log('formLayout', formLayout)
     console.log('temp', temp)
 
-    let formWrap = styles.formWrap
+    let {formWrap} = styles
     let className = 'vertical-form'
     if (formLayout === 'inline') {
       className = ''
